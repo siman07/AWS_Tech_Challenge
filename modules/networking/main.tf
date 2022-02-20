6496ad41bd388d72ea7fac513c669d2f71f321e3
siman07 <80423977+siman07@users.noreply.github.com>
2022-02-20 15:09:27 -0500

    push to main repo
    pushing challenge project

---
+++ modules/networking/main.tf
@@ -0,0 +1,75 @@
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name                             = "${var.namespace}-vpc"
+  cidr                             = "10.1.0.0/16"
+  azs                              = data.aws_availability_zones.available.names
+  private_subnets                  = ["10.1.2.0/24", "10.1.3.0/24"]
+  public_subnets                   = ["10.1.0.0/24", "10.1.1.0/24"]
+  #assign_generated_ipv6_cidr_block = false
+  create_database_subnet_group     = false
+  enable_nat_gateway               = false
+  single_nat_gateway               = false
+}
+
+// SG to allow SSH connections from anywhere
+resource "aws_security_group" "allow_ssh_pub" {
+  name        = "${var.namespace}-allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "SSH from the internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.namespace}-allow_ssh_pub"
+  }
+}
+
+// SG to onlly allow SSH connections from VPC public subnets
+resource "aws_security_group" "allow_ssh_priv" {
+  name        = "${var.namespace}-allow_ssh_priv"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "SSH only from internal VPC clients"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.1.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.namespace}-allow_ssh_priv"
+  }
+}
+# Creating Internet Gateway in AWS VPC
+resource "aws_internet_gateway" "vpc-gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "vpc"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "namespace" {
+  description = "The project namespace to use for unique resource naming"
+  default     = "AWS_Tech"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+  type        = string
